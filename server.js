8c687b5d3db52c622b23b002935f664edbfbfeb3
Marina Smirnova <marina.smirnova.13.07@gmail.com>
2022-01-08 10:08:05 +0300

    fix timeout

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT||3000;
 
 app.use(express.static(path.join(__dirname,'build')));
-app.use(timeout('5s'));
+app.use(timeout('15s'));
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'build','index.html'));
 });
